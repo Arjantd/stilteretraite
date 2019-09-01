57a9dfb7f77a0cfb8d3c5e407f15ab42b790d06b
Oscar van der Leij <oscar.vanderleij@magnaversum.com>
2019-09-01 13:10:46 +0200

    Fixed youtube video

--- _videos/wat-is-een-stilte-retraite.md
+++ _videos/wat-is-een-stilte-retraite.md
@@ -1,6 +1,6 @@
 ---
 template: video
-link: https://www.youtube.com/watch?v=1aq8JSiKmLs&t=84s
+link: https://www.youtube.com/watch?v=1aq8JSiKmLs
 thumbnail: https://static.stilteretraite.nl/uploads/Avant Spirit - Velp 1.jpg
 caption: Wat is een stilte retraite?
 
--- js/init.js
+++ js/init.js
@@ -259,7 +259,30 @@ var BORNTOGIVE = window.BORNTOGIVE || {};
 		});
 		jQuery('.magnific-video').magnificPopup({ 
 			type: 'iframe',
-			gallery:{enabled:false}
+			gallery:{enabled:false},
+			iframe: {
+				patterns: {
+					youtube: {
+						index: 'youtube.com/', 
+						id: function(url) {
+							var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+							if ( !m || !m[1] ) return null;
+							console.log(m)
+							return m[1];
+						},
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					},
+					vimeo: {
+						index: 'vimeo.com/', 
+						id: function(url) {        
+							var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+							if ( !m || !m[5] ) return null;
+							return m[5];
+						},
+						src: '//player.vimeo.com/video/%id%?autoplay=1'
+					}
+				}
+			}
 			// other options
 		});
 	}
