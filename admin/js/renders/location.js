c79979c46631bfbdd4cbf452b3b7600e369f086d
Oscar van der Leij <oscar.vanderleij@gmail.com>
2023-11-20 17:42:40 +0100

    Added location

---
+++ admin/js/renders/location.js
@@ -0,0 +1,23 @@
+export function locationRender() {
+    var entry = this.props.entry;
+    var image = entry.getIn(['data', 'image']);
+    var asset_image = this.props.getAsset(image);
+
+    return h('div', {className: 'container'},
+        h('div', {className: 'row'},
+            h('div', {className: 'col-md-8 col-md-offset-2'},
+                h('h1', {}, entry.getIn(['data', 'title'])),
+                h('img', {src: asset_image.toString(), width: 750, height: 500}),
+                h('div', {className: 'spacer-20'}),
+                this.props.widgetFor('body'),
+                h('div', {className: 'spacer-20'}),
+                h('div', {className: 'row gallery-items-nocaption'}),
+                this.props.widgetsFor('gallery').map((gallery_item, index) => 
+                    h('div', {key: index, className: 'col-md-4 col-sm-6 col-xs-6 gallery-grid-item'},
+                        h('a', {href: gallery_item})
+                    )
+                )
+            )
+        )
+    )
+}
