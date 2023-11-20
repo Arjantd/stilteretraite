5a7d0a0ddf8a7dc552892a2cb27226b014c61640
Oscar van der Leij <oscar.vanderleij@gmail.com>
2023-11-20 17:08:04 +0100

    Preview render for person

---
+++ admin/js/renders/person.js
@@ -0,0 +1,11 @@
+export function personRender() {
+    var entry = this.props.entry;
+    var thumbnail = entry.getIn(['data', 'thumbnail']);
+    var asset_thumbnail = this.props.getAsset(thumbnail);
+
+    return h('div', {className: 'container'},
+      h('h3', {}, entry.getIn(['data', 'title'])),
+      h('img', {src: asset_thumbnail.toString(), width: 750, height: 500}),
+      h('div', {}, this.props.widgetFor('body'))
+    )
+  }
