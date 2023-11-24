79b9744a25f2decf7295f8c263552f1cce986e4f
Oscar van der Leij <oscar.vanderleij@gmail.com>
2023-11-24 14:45:42 +0100

    Do not encode url

--- admin/js/image.js
+++ admin/js/image.js
@@ -1,5 +1,3 @@
 export function resizedImageUrl(url, width, height, fit) {
-    let uri = new URL(encodeURI(url));
-    let fitString = fit ? `-${fit}` : '';
     return `/.netlify/images?url=/${url}&fit=${fit}&w=${width}&h=${height}`
 }
