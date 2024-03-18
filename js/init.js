7db72bbb0db3ddf4f81291f7e96ce84987fbfbf0
Oscar van der Leij <oscar.vanderleij@gmail.com>
2024-03-18 16:24:31 +0100

    Moved nettlify javascript

--- js/init.js
+++ js/init.js
@@ -705,3 +705,13 @@ if ($.fancybox) {
 		ZOOM: "Inzoomen"
 	}
 }
+
+if (window.netlifyIdentity) {
+	window.netlifyIdentity.on("init", user => {
+	  if (!user) {
+		window.netlifyIdentity.on("login", () => {
+		  document.location.href = "/admin/";
+		});
+	  }
+	});
+}
