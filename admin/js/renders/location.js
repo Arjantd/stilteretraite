2b36aa8843badea3d9358af2b4a37bf8c6908e3d
Oscar van der Leij <oscar.vanderleij@gmail.com>
2024-12-08 20:45:16 +0100

    Fixed location issue when no items in gallery

--- admin/js/renders/location.js
+++ admin/js/renders/location.js
@@ -4,7 +4,7 @@ export function locationRender() {
     var entry = this.props.entry;
     var image = entry.getIn(['data', 'image']);
     var asset_image = this.props.getAsset(image);
-console.log(this.props.widgetsFor('gallery'))
+
     return h('div', {className: 'container'},
         h('div', {className: 'row'},
             h('div', {className: 'col-md-8 col-md-offset-2'},
@@ -15,9 +15,10 @@ console.log(this.props.widgetsFor('gallery'))
                 h('div', {className: 'spacer-20'}),
                 h('div', {className: 'row gallery-items-nocaption'},
                     this.props.widgetsFor('gallery').map((gallery_item, index) =>
+                        gallery_item ? 
                         ('div', {key: index, className: 'col-md-4 col-sm-6 col-xs-6 gallery-grid-item'},
                             h('img', {src: resizedImageUrl(gallery_item.get('data').toString(), 358, 239), width: 358, height: 239})
-                        )
+                        ) : null
                     )
                 )
             )
